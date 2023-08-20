b61e1c2996ab7bf7bc8603a45432162127c25936
Higor Sales <142263535+higorsalessilva@users.noreply.github.com>
2023-08-20 13:03:39 -0300

    D001 JavaScript

---
+++ desafios/d001/script.js
@@ -0,0 +1,11 @@
+function b1() {
+    window.alert('Você clicou no 1° botão!')
+}
+
+function b2() {
+    window.alert('Você clicou no 2° botão!')
+}
+
+function b3() {
+    window.alert('Você clicou no 3° botão!')
+}
