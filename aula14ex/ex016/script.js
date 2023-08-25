04be9c4c4203a06cc25a95960765cedfdf0474c2
Higor Sales <142263535+higorsalessilva@users.noreply.github.com>
2023-08-25 09:55:36 -0300

    Exercicios JavaScript (parte4)
    ex016 (falta conferir em video)

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,11 +1,28 @@
 var inc = document.querySelector('input#inc')
 var fim = document.querySelector('input#fim')
+var pss = document.querySelector('input#pss')
 var area = document.getElementsByTagName('div')[1]
 function iniciar() {
     var i = Number(inc.value)
     var f = Number(fim.value)
-    while (i <= f) {
-        area.innerHTML += `${i}`
-        i++
+    var p = Number(pss.value)
+    
+    if (i != 0 && f != 0 && p != 0) {
+        area.innerHTML = 'Contando:<br>'
+        while (i <= f) {
+            area.innerHTML += `${i} &#X1F449;`
+            i = i + p 
+        }
+        area.innerHTML += '&#X1F3C1'
+    } else if (p == 0) {
+        window.alert('Passo invalido! Considerando PASSO 1')
+        area.innerHTML = 'Contando:<br>'
+        while (i <= f) {
+            area.innerHTML += `${i} &#X1F449;`
+            i++ 
+        }
+        area.innerHTML += '&#X1F3C1'
+    } else {
+        area.innerHTML = 'Impossível contar!'
     }
 }
