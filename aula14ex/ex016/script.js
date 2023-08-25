0bcbcdade3725e31db3ffbcac258f98b5f4c4213
Higor Sales <142263535+higorsalessilva@users.noreply.github.com>
2023-08-25 15:39:21 -0300

    Exercícios JavaScript (parte 5)
    Conferindo e corrigindo ex016 JS

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,3 +1,37 @@
+function iniciar() {
+    let ini = document.getElementById('inc')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('pss')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')        
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+         res.innerHTML += `\u{1F3C1}`
+    }
+}
+
+
+/*
+ MEU CODIGO
 var inc = document.querySelector('input#inc')
 var fim = document.querySelector('input#fim')
 var pss = document.querySelector('input#pss')
@@ -6,23 +40,36 @@ function iniciar() {
     var i = Number(inc.value)
     var f = Number(fim.value)
     var p = Number(pss.value)
-    
-    if (i != 0 && f != 0 && p != 0) {
-        area.innerHTML = 'Contando:<br>'
-        while (i <= f) {
-            area.innerHTML += `${i} &#X1F449;`
-            i = i + p 
-        }
-        area.innerHTML += '&#X1F3C1'
-    } else if (p == 0) {
-        window.alert('Passo invalido! Considerando PASSO 1')
-        area.innerHTML = 'Contando:<br>'
-        while (i <= f) {
-            area.innerHTML += `${i} &#X1F449;`
-            i++ 
-        }
-        area.innerHTML += '&#X1F3C1'
-    } else {
+
+    area.innerHTML = 'Contando: <br>'
+    if (i === 0 || f === 0) {
         area.innerHTML = 'Impossível contar!'
+    } else if (p === 0) {
+        window.alert('Passo invalido! Considerando PASSO 1')
+        if (i < f) {
+            while (i <= f) {
+                area.innerHTML += `${i} &#X1F449;`
+                i++
+            }
+            area.innerHTML += '&#X1F3C1;'
+        } else {
+            while (i >= f) {
+                area.innerHTML += `${i} &#X1F449;`
+                i--
+            }
+            area.innerHTML += '&#X1F3C1;'
+        }
+    } else if (i < f) {
+        do {
+            area.innerHTML += `${i} &#X1F449;`
+            i = i + p
+        } while (i <= f)
+        area.innerHTML += '&#X1F3C1;'
+    }  else {
+        for (i; i >= f; i = i - p) {
+            area.innerHTML += `${i} &#X1F449;`
+        }
+        area.innerHTML += '&#X1F3C1;'
     }
-}
+}   
+*/
