5c34159f46f8969c1879aedc965c85def6880dd5
Higor Sales <142263535+higorsalessilva@users.noreply.github.com>
2023-08-25 13:01:01 -0300

    Exercicios JavaScript (parte4)
    ex017 modelo antes da correção

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,12 @@
-alert('olá')
+var num = document.querySelector('input#num')
+area = document.getElementsByTagName('select')[0]
+function gerar() {
+    var n = Number(num.value)
+    if (n != null) {
+        for (var m = 1; m <= 10; m++) {
+            area.innerHTML += `<option>${n} x ${m} = ${n*m}</option>`
+        }
+    } else {
+        window.alert('Por favor, digite um número!')
+    }
+}
